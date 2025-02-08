fa81565d95d666b075f894a117f0b32565a2eae7
Beatriz <bbazagli@student.42sp.org.br>
2025-02-08 12:00:29 -0300

    fixed ex01

--- Module 07/ex01/include/iter.hpp
+++ Module 07/ex01/include/iter.hpp
@@ -3,15 +3,15 @@
 
 #include <iostream>
 
-template <typename T, typename Func>
-void iter(T* arr, size_t len, Func func) {
+template <typename T>
+void iter(T* arr, size_t len, void (*func)(T &)) {
     for (size_t i = 0; i < len; ++i) {
         func(arr[i]);  
     }
 }
 
 template <typename T>
-void printElement(T element) {
+void printElement(T &element) {
     std::cout << element << " ";
 }
 
