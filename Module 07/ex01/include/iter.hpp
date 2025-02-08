9455311e9b5ee13dfd0dcc63f392828b8cbc686f
Beatriz <bbazagli@student.42sp.org.br>
2025-02-08 12:01:58 -0300

    improved func signature for ex01

--- Module 07/ex01/include/iter.hpp
+++ Module 07/ex01/include/iter.hpp
@@ -4,7 +4,7 @@
 #include <iostream>
 
 template <typename T>
-void iter(T* arr, size_t len, void (*func)(T &)) {
+void iter(T* arr, size_t len, void (*func)(T &element)) {
     for (size_t i = 0; i < len; ++i) {
         func(arr[i]);  
     }
