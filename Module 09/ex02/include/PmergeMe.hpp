db5c8cb6111f67d051e80bb59bdd0b0919692fcf
Beatriz Bazaglia <beatriz@bazaglia.com>
2025-04-11 15:02:19 -0300

    new version using iterator instead of std::pair

--- Module 09/ex02/include/PmergeMe.hpp
+++ Module 09/ex02/include/PmergeMe.hpp
@@ -8,20 +8,36 @@
 #include <algorithm>
 #include <iostream>
 
-class PmergeMe {
-private:
-    std::vector<int> m_originalSequence;
-    std::vector<int> m_vectorSorted;
-    std::deque<int> m_dequeSorted;
-    double m_vectorTime;
-    double m_dequeTime;
+// utils 
+template <typename Iterator> bool _compare(Iterator lv, Iterator rv);
+template <typename Iterator> Iterator advance_iterator_by(Iterator it, int steps);
+template <typename Iterator> void _swap_with_next_pair(Iterator rightmost_element_it, int pair_group_size);
+template <typename Container> void print_container(Container& container);
 
-    bool isPositiveInteger(const std::string &str) const;
-    std::vector<size_t> generateJacobsthalNumbers(size_t n) const;
-    std::vector<size_t> getInsertionSequence(size_t n) const;
-    
-    template <typename Container>
-    void fordJohnsonSort(Container &container);
+class PmergeMe
+{
+private:
+    std::vector<int> original_sequence;
+    std::vector<int> vector;
+    std::deque<int> deque;
+    double vector_time;
+    double deque_time;
+
+    template <typename Container, typename Chain>
+    void fordJohnsonSort(Container &container, int group_size);
+    template <typename Container, typename Chain>
+    void _merge_phase(Container &container, int group_size);
+    template <typename Container, typename Chain>
+    void _create_chains(Container &container, int group_size, Chain &main, Chain &pend);
+    template <typename Container, typename Chain>
+    std::size_t _optimized_insertion(Chain &main, Chain &pend);
+    template <typename Container, typename Chain>
+    void _insert_remaining(std::size_t container_size, int group_size, Chain &main, Chain &pend, std::size_t processed_pend_elements);
+    template <typename Container, typename Chain>
+    void _update_container(Container &container, Chain &main, int group_size);
+
+    static std::size_t _jacobsthal_cache[62];
+    static std::size_t _calculate_jacobsthal_number(std::size_t n);
 
 public:
     PmergeMe();
@@ -29,163 +45,184 @@ public:
     PmergeMe &operator=(const PmergeMe &other);
     ~PmergeMe();
 
+    static int comparison_counter;
+
     void parseArguments(int argc, char *argv[]);
     void sort();
     void displayResults() const;
-
-    // Getters
-    const std::vector<int> &getOriginalSequence() const;
-    const std::vector<int> &getVectorSorted() const;
-    const std::deque<int> &getDequeSorted() const;
-    double getVectorTime() const;
-    double getDequeTime() const;
 };
 
+template <typename Container, typename Chain>
+void PmergeMe::fordJohnsonSort(Container &container, int group_size)
+{
+    _merge_phase<Container, Chain>(container, group_size);
 
-template <typename T1, typename T2>
-std::ostream& operator<<(std::ostream& os, const std::pair<T1, T2>& p) {
-    os << "(" << p.first << ", " << p.second << ")";
-    return os;
-} 
+    Chain main;
+    Chain pend;
 
-template <typename Container>
-void print_container(Container& container) {
-	for (typename Container::iterator it = container.begin(); it != container.end(); it++) {
-		std::cout << *it << " ";
-	}
-	std::cout << std::endl;
+    _create_chains<Container, Chain>(container, group_size, main, pend);
+
+    std::size_t processed_pend_elements = _optimized_insertion<Container, Chain>(main, pend);
+
+    _insert_remaining<Container, Chain>(container.size(), group_size, main, pend, processed_pend_elements);
+
+    _update_container<Container, Chain>(container, main, group_size);
 }
 
+template <typename Container, typename Chain>
+void PmergeMe::_merge_phase(Container &container, int group_size)
+{
+    typedef typename Container::iterator Iterator;
 
-template <typename Container>
-void PmergeMe::fordJohnsonSort(Container &arr) {
-    const size_t size = arr.size();
-    if (size <= 1)
+    int group_count = container.size() / group_size;
+
+    if (group_count < 2)
         return;
 
-    // Step 1: Make pairs and sort them internally
-    std::vector<std::pair<int, int> > pairs;
-    bool isOdd = (size % 2 != 0);
-    int oddNum = 0;
+    int num_group_pairs = group_count / 2;
 
-    if (isOdd) 
-        oddNum = arr[size - 1];
+    Iterator merge_start = container.begin();
+    Iterator merge_end = advance_iterator_by(container.begin(), group_size * 2 * num_group_pairs);
 
-    // Create pairs and ensure smaller is first, larger is second
-    for (size_t i = 0; i < size - isOdd; i += 2) {
-        int first = arr[i];
-        int second = arr[i + 1];
-        if (first > second)
-            std::swap(first, second);
-        pairs.push_back(std::make_pair(first, second));
-        std::cout << "Created pair: (" << first << ", " << second << ")" << std::endl;
+    int merge_step = 2 * group_size;
+    for (Iterator it = merge_start; it != merge_end; std::advance(it, merge_step)) {
+        Iterator current_group_largest_element = advance_iterator_by(it, group_size - 1);
+        Iterator next_group_largest_element = advance_iterator_by(it, group_size * 2 - 1);
+
+        if (_compare(next_group_largest_element, current_group_largest_element))
+            _swap_with_next_pair(current_group_largest_element, group_size);
     }
 
-    // Step 2: Extract larger elements for recursive sorting
-    Container largerElements;
-    for (size_t i = 0; i < pairs.size(); ++i) {
-        largerElements.push_back(pairs[i].second);
-    }
-
-    std::cout << "recursion now" << std::endl;
-    // Recursively sort the larger elements
-    fordJohnsonSort(largerElements);
-
-    // Step 3: Initialize main chain with first pair elements
-    Container sortedArr;
-
-    // Create a mapping array to track which pair each larger element came from
-    std::vector<size_t> pairIndices(pairs.size());
-    std::vector<bool> used(pairs.size(), false);
-
-    // Match sorted larger elements back to their original pairs
-    for (size_t i = 0; i < largerElements.size(); ++i) {
-        for (size_t j = 0; j < pairs.size(); ++j) {
-            if (!used[j] && pairs[j].second == largerElements[i]) {
-                pairIndices[i] = j;
-                used[j] = true;
-                break;
-            }
-        }
-    }
-
-    // Initialize with first pair
-    sortedArr.push_back(pairs[pairIndices[0]].first);
-    sortedArr.push_back(pairs[pairIndices[0]].second);
-
-    std::cout << "Initialized sorted array with first pair: (" 
-              << pairs[pairIndices[0]].first << ", " 
-              << pairs[pairIndices[0]].second << ")" << std::endl;
-
-
-    // Insert all remaining larger elements in the main chain
-    for (size_t i = 1; i < largerElements.size(); ++i) {
-        sortedArr.push_back(largerElements[i]);
-    }
-    std::cout << "Main chain: " << std::endl;
-    print_container(sortedArr);
-
-    // Create pending insertions list
-    std::vector<std::pair<int, size_t> > pendingElements;
-    for (size_t i = 1; i < pairs.size(); ++i) {
-        pendingElements.push_back(std::make_pair(pairs[pairIndices[i]].first, i));
-    }
-    std::cout << "Pending Elements: " << std::endl;
-    print_container(pendingElements);
-
-    // Get the Jacobsthal insertion sequence
-    std::vector<size_t> insertionSequence = getInsertionSequence(pendingElements.size());
-
-    // Insert remaining elements according to the sequence
-    for (size_t i = 0; i < insertionSequence.size(); ++i) {
-        size_t idx = insertionSequence[i];
-        if (idx < pendingElements.size()) {
-            int valueToInsert = pendingElements[idx].first;
-
-            // Find insertion position using binary search
-            typename Container::iterator lower = sortedArr.begin();
-            typename Container::iterator upper = sortedArr.end();
-
-            while (lower != upper) {
-                typename Container::iterator mid = lower;
-                std::advance(mid, std::distance(lower, upper) / 2);
-
-                if (*mid < valueToInsert) {
-                    lower = mid;
-                    ++lower;
-                } else {
-                    upper = mid;
-                }
-            }
-
-            sortedArr.insert(lower, valueToInsert);
-            std::cout << "Inserted " << valueToInsert << " into sorted array." << std::endl;
-        }
-    }
-
-    // Insert oddNum if exists
-    if (isOdd) {
-        typename Container::iterator lower = sortedArr.begin();
-        typename Container::iterator upper = sortedArr.end();
-
-        while (lower != upper) {
-            typename Container::iterator mid = lower;
-            std::advance(mid, std::distance(lower, upper) / 2);
-
-            if (*mid < oddNum) {
-                lower = mid;
-                ++lower;
-            } else {
-                upper = mid;
-            }
-        }
-
-        sortedArr.insert(lower, oddNum);
-        std::cout << "Inserted " << oddNum << " into sorted array." << std::endl;
-    }
-
-    // Copy sorted array back to original
-    arr = sortedArr;
+    fordJohnsonSort<Container, Chain>(container, group_size * 2);
 }
+
+template <typename Container, typename Chain>
+void PmergeMe::_create_chains(Container &container, int group_size, Chain &main, Chain &pend)
+{
+    typedef typename Container::iterator Iterator;
+
+    int group_count = container.size() / group_size;
+
+    for (int i = 0; i < group_count; i++) {
+        int largest_element_position = (group_size * i) + group_size - 1;
+        Iterator current = advance_iterator_by(container.begin(), largest_element_position);
+
+        if (i == 0 || i % 2 == 1)
+            main.push_back(current);
+        else
+            pend.push_back(current);
+    }
+}
+
+template <typename Container, typename Chain>
+std::size_t PmergeMe::_optimized_insertion(Chain &main, Chain &pend)
+{
+    typedef typename Container::iterator Iterator;
+    typedef typename Chain::iterator ChainIterator;
+
+    std::size_t processed_pend_elements = 0;
+    for (std::size_t jacob_index = 2; processed_pend_elements < pend.size(); jacob_index++) {
+        std::size_t curr_jacobsthal = _calculate_jacobsthal_number(jacob_index);
+        std::size_t group_size = _calculate_jacobsthal_number(jacob_index) - _calculate_jacobsthal_number(jacob_index - 1);
+        if (group_size > pend.size() - processed_pend_elements)
+            break;
+
+        std::size_t elements_to_insert = group_size;
+        int overlap_correction = 0;
+
+        ChainIterator pend_it = advance_iterator_by(pend.begin() + processed_pend_elements, (elements_to_insert - 1));
+        while (elements_to_insert--) {
+            ChainIterator search_bound = advance_iterator_by(main.begin(), _calculate_jacobsthal_number(jacob_index) + processed_pend_elements - overlap_correction);
+
+            ChainIterator insertion_point = std::upper_bound(main.begin(), search_bound, *pend_it, _compare<Iterator>);
+            ChainIterator inserted = main.insert(insertion_point, *pend_it);
+
+            if (std::distance(main.begin(), inserted) == static_cast<std::ptrdiff_t>(curr_jacobsthal + processed_pend_elements))
+                overlap_correction++;
+
+            std::advance(pend_it, -1);
+        }
+        processed_pend_elements += group_size;
+    }
+    return (processed_pend_elements);
+}
+
+template <typename Container, typename Chain>
+void PmergeMe::_insert_remaining(std::size_t container_size, int group_size, Chain &main, Chain &pend, std::size_t processed_pend_elements)
+{
+    typedef typename Container::iterator Iterator;
+    typedef typename Chain::iterator ChainIterator;
+
+    int group_count = container_size / group_size;
+
+    bool is_odd = group_count % 2 == 1;
+    for (size_t index = processed_pend_elements; index < pend.size(); index++) {
+        ChainIterator current = advance_iterator_by(pend.begin(), index);
+        ChainIterator bound = advance_iterator_by(main.begin(), main.size() - pend.size() + index + is_odd);
+
+        ChainIterator insertion_point = std::upper_bound(main.begin(), bound, *current, _compare<Iterator>);
+        main.insert(insertion_point, *current);
+    }
+}
+
+template <typename Container, typename Chain>
+void PmergeMe::_update_container(Container &container, Chain &main, int group_size)
+{
+    typedef typename Container::iterator Iterator;
+    typedef typename Chain::iterator ChainIterator;
+
+    Container sorted;
+    for (ChainIterator it = main.begin(); it != main.end(); it++) {
+        for (int group_pos = 0; group_pos < group_size; group_pos++) {
+            Iterator group_member_it = *it;
+            std::advance(group_member_it, -group_size + group_pos + 1);
+            sorted.push_back(*group_member_it);
+        }
+    }
+
+    std::copy(sorted.begin(), sorted.end(), container.begin());
+}
+
+// utils
+template <typename T1, typename T2>
+std::ostream &operator<<(std::ostream &os, const std::pair<T1, T2> &p)
+{
+    os << "(" << p.first << ", " << p.second << ")";
+    return os;
+}
+
+template <typename Container>
+void print_container(Container &container)
+{
+    for (typename Container::const_iterator it = container.begin(); it != container.end(); it++)
+        std::cout << *it << " ";
+    std::cout << std::endl;
+}
+
+template <typename Iterator>
+Iterator advance_iterator_by(Iterator it, int steps)
+{
+    std::advance(it, steps);
+    return it;
+}
+
+template <typename Iterator>
+bool _compare(Iterator left_value, Iterator right_value)
+{
+    return *left_value < *right_value;
+}
+
+template <typename Iterator>
+void _swap_with_next_pair(Iterator last_element_of_first_group, int group_size)
+{
+    Iterator current_group_start = advance_iterator_by(last_element_of_first_group, -group_size + 1);
+    Iterator next_group_start = advance_iterator_by(current_group_start, group_size);
+
+    while (current_group_start != next_group_start) {
+        std::iter_swap(current_group_start, advance_iterator_by(current_group_start, group_size));
+        current_group_start++;
+    }
+}
+
 
 #endif // PMERGE_ME_HPP
--- Module 09/ex02/src/PmergeMe.cpp
+++ Module 09/ex02/src/PmergeMe.cpp
@@ -5,187 +5,106 @@
 #include <ctime>
 #include <cstdlib>
 
-PmergeMe::PmergeMe() : m_vectorTime(0), m_dequeTime(0) {}
+std::size_t PmergeMe::_jacobsthal_cache[62] = {0};
 
-PmergeMe::~PmergeMe() {}
+PmergeMe::PmergeMe() : vector_time(0), deque_time(0) {}
 
-// Copy constructor
 PmergeMe::PmergeMe(const PmergeMe &other)
-    : m_originalSequence(other.m_originalSequence),
-      m_vectorSorted(other.m_vectorSorted),
-      m_dequeSorted(other.m_dequeSorted),
-      m_vectorTime(other.m_vectorTime),
-      m_dequeTime(other.m_dequeTime) {}
+{
+    *this = other;
+}
 
-// Assignment operator
-PmergeMe &PmergeMe::operator=(const PmergeMe &other) {
-    if (this != &other) {
-        m_originalSequence = other.m_originalSequence;
-        m_vectorSorted = other.m_vectorSorted;
-        m_dequeSorted = other.m_dequeSorted;
-        m_vectorTime = other.m_vectorTime;
-        m_dequeTime = other.m_dequeTime;
+PmergeMe &PmergeMe::operator=(const PmergeMe &other)
+{
+    if (this != &other)
+    {
+        original_sequence = other.original_sequence;
+        vector = other.vector;
+        deque = other.deque;
+        vector_time = other.vector_time;
+        deque_time = other.deque_time;
     }
     return *this;
 }
 
-void PmergeMe::parseArguments(int argc, char* argv[]) {
-    m_originalSequence.clear();
+PmergeMe::~PmergeMe() {}
 
-    if (argc < 2) {
+void PmergeMe::parseArguments(int argc, char *argv[])
+{
+    original_sequence.clear();
+
+    if (argc < 2)
+    {
         std::cerr << "Usage: " << argv[0] << " [positive integers...]" << std::endl;
         std::exit(1);
     }
 
-    for (int i = 1; i < argc; i++) {
+    for (int i = 1; i < argc; i++)
+    {
         std::string arg(argv[i]);
 
-        for (size_t j = 0; j < arg.length(); ++j) {
-            if (!std::isdigit(arg[j])) {
+        for (size_t j = 0; j < arg.length(); ++j)
+        {
+            if (!std::isdigit(arg[j]))
+            {
                 std::cerr << "Error: Invalid input '" << arg << "'. Expected a positive integer." << std::endl;
                 std::exit(1);
             }
         }
 
         long num = std::atol(arg.c_str());
-        if (num > INT_MAX) {
+        if (num > INT_MAX)
+        {
             std::cerr << "Error: " << num << " exceeds INT_MAX." << std::endl;
             std::exit(1);
         }
 
-        // m_originalSequence contains the input sequence provided by the user
-        m_originalSequence.push_back(static_cast<int>(num));
-
-        // std::cout << "Added " << num << " to m_originalSequence" << std::endl;
+        original_sequence.push_back(static_cast<int>(num));
     }
 }
 
+std::size_t PmergeMe::_calculate_jacobsthal_number(std::size_t n)
+{
+    if (std::size_t cache = _jacobsthal_cache[n])
+        return cache;
 
-// Generate Jacobsthal numbers up to n
-std::vector<size_t> PmergeMe::generateJacobsthalNumbers(size_t n) const {
-    std::vector<size_t> jacobsthal;
+    std::size_t result = round((pow(2, n + 1) + pow(-1, n)) / 3);
+    _jacobsthal_cache[n] = result;
 
-    if (n == 0)
-        return jacobsthal;
-
-    // Start with the first two Jacobsthal numbers
-    jacobsthal.push_back(0);
-    if (n == 1)
-        return jacobsthal;
-
-    jacobsthal.push_back(1);
-
-    // Generate Jacobsthal numbers until we exceed n
-    size_t i = 2;
-    while (true) {
-        size_t next = jacobsthal[i - 1] + 2 * jacobsthal[i - 2];
-        if (next > n)
-            break;
-
-        jacobsthal.push_back(next);
-
-        // std::cout << "Generated Jacobsthal number: " << next << std::endl;
-
-        i++;
-    }
-
-    return jacobsthal;
-}
-
-// Get insertion sequence based on Jacobsthal numbers
-std::vector<size_t> PmergeMe::getInsertionSequence(size_t n) const {
-    if (n == 0)
-        return std::vector<size_t>();
-
-    std::vector<size_t> jacobsthal = generateJacobsthalNumbers(n);
-    std::vector<size_t> sequence;
-
-    // Handle the case of a single element separately
-    if (n == 1) {
-        sequence.push_back(1);
-        return sequence;
-    }
-
-    // Create a sequence based on Jacobsthal numbers
-    // Skip 0, we start with element at index 1
-    for (size_t i = 1; i < jacobsthal.size(); i++) {
-        size_t current = jacobsthal[i];
-
-        // Add the current Jacobsthal number
-        if (current <= n)
-            sequence.push_back(current);
-
-        // Add numbers between this and the previous Jacobsthal number in reverse
-        for (size_t j = current - 1; j > jacobsthal[i - 1]; j--) {
-            if (j <= n)
-                sequence.push_back(j);
-        }
-    }
-
-    // Add any remaining numbers in sequence
-    size_t last = jacobsthal.back();
-    for (size_t i = last + 1; i <= n; i++) {
-        sequence.push_back(i);
-    }
-
-    return sequence;
+    return result;
 }
 
 // Sort the sequence using both container types
-void PmergeMe::sort() {
-    m_vectorSorted = m_originalSequence;
-    m_dequeSorted.assign(m_originalSequence.begin(), m_originalSequence.end());
+void PmergeMe::sort()
+{
+    vector = original_sequence;
+    deque.assign(original_sequence.begin(), original_sequence.end());
 
     // Sort with vector and measure time
     clock_t startVector = clock();
-    fordJohnsonSort(m_vectorSorted);
+    fordJohnsonSort<std::vector<int>, std::vector<std::vector<int>::iterator> >(vector, 1);
     clock_t endVector = clock();
-    m_vectorTime = static_cast<double>(endVector - startVector) / CLOCKS_PER_SEC * 1000000; // µs
+    vector_time = static_cast<double>(endVector - startVector) / CLOCKS_PER_SEC * 1000000; // µs
 
     // Sort with deque and measure time
     clock_t startDeque = clock();
-    fordJohnsonSort(m_dequeSorted);
+    fordJohnsonSort<std::deque<int>, std::deque<std::deque<int>::iterator> >(deque, 1);
     clock_t endDeque = clock();
-    m_dequeTime = static_cast<double>(endDeque - startDeque) / CLOCKS_PER_SEC * 1000000; // µs
+    deque_time = static_cast<double>(endDeque - startDeque) / CLOCKS_PER_SEC * 1000000; // µs
 }
 
-void PmergeMe::displayResults() const {
+void PmergeMe::displayResults() const
+{
     std::cout << "Before: ";
-    for (size_t i = 0; i < m_originalSequence.size(); i++) {
-        std::cout << m_originalSequence[i] << " ";
-    }
-    std::cout << std::endl;
+    print_container(original_sequence);
 
     std::cout << "After: ";
-    for (size_t i = 0; i < m_vectorSorted.size(); i++) {
-        std::cout << m_vectorSorted[i] << " ";
-    }
+    print_container(vector);
     std::cout << std::endl;
 
-    std::cout << "Time to process a range of " << m_originalSequence.size()
-              << " elements with std::vector : " << m_vectorTime << " us" << std::endl;
+    std::cout << "Time to process a range of " << original_sequence.size()
+              << " elements with std::vector : " << vector_time << " us" << std::endl;
 
-    std::cout << "Time to process a range of " << m_originalSequence.size()
-              << " elements with std::deque : " << m_dequeTime << " us" << std::endl;
-}
-
-// Getters
-const std::vector<int> &PmergeMe::getOriginalSequence() const {
-    return m_originalSequence;
-}
-
-const std::vector<int> &PmergeMe::getVectorSorted() const {
-    return m_vectorSorted;
-}
-
-const std::deque<int> &PmergeMe::getDequeSorted() const {
-    return m_dequeSorted;
-}
-
-double PmergeMe::getVectorTime() const {
-    return m_vectorTime;
-}
-
-double PmergeMe::getDequeTime() const {
-    return m_dequeTime;
+    std::cout << "Time to process a range of " << original_sequence.size()
+              << " elements with std::deque : " << deque_time << " us" << std::endl;
 }
